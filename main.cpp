e65c632d7d30bac71a7a01abbb50f0d638170f7d
liutt1312 <liutt1312@126.com>
2022-05-08 19:29:35 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
+	cout<<"fileName: "<<__FILE__;
+	cout<<" funName: "<<__func__;
+	cout<<" Line: "<<__LINE__<<endl;
+
 	cout<<"hello world !!!"<<endl;
+
 	return 0;
 }
